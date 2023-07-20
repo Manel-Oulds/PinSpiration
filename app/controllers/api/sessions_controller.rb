c902d123cd9ca1a21d1f872b9195777fb7ead28b
Manel-Os <oulds.manel@gmail.com>
2023-07-20 17:25:32 -0400

    user auth ...

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,8 +2,7 @@ class Api::SessionsController < ApplicationController
   def show
     if current_user
       @user = current_user
-      # render "api/views/show"
-      render json: {user: @user}
+      render 'api/users/show'
     else
       render json: { user: nil }
     end
@@ -13,8 +12,7 @@ class Api::SessionsController < ApplicationController
     @user = User.find_by_credentials(params[:credential],params[:password])
     if @user
       login!(@user)
-      # render "api/view/show"
-      render json: {user: @user}
+      render "api/users/show"
     else
       render json: { errors: ['Incorrect Email/Username or Password.'] }, 
       status: :unauthorized
