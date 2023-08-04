1d097084be4172e89f3034b2a359315a2cf4e845
Manel-Os <oulds.manel@gmail.com>
2023-08-03 21:32:08 -0400

    Added Favicon

--- frontend/src/components/SearchResult/index.js
+++ frontend/src/components/SearchResult/index.js
@@ -1,11 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import { fetchSearchResults } from "../../store/search";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import Navigation from "../Navigation";
-import { ClassNames } from "@emotion/react";
+import Modal from "../context/Modal";
+import { ShowPinItem } from "../ShowPinItem";
 function SearchPage() {
+  const [selectedPin, setSelectedPin] = useState(null);
+  const [showModal, setShowModal] = useState(false);
   const dispatch = useDispatch();
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
@@ -18,6 +21,16 @@ function SearchPage() {
     const randomIndex = Math.floor(Math.random() * sizes.length);
     return sizes[randomIndex];
   }
+  const handleModalClose = () => {
+    setShowModal(false);
+  };
+
+  const handleImageClick = (pin) => (event) => {
+    setSelectedPin(pin);
+
+    setShowModal(true);
+  };
+
   useEffect(() => {
     dispatch(fetchSearchResults(query));
   }, []);
@@ -26,11 +39,19 @@ function SearchPage() {
       <Navigation />
       <div className="container">
         {searchresults?.map((item) => (
-          <div className={`${getRandomSize()}`}>
+          <div
+            className={`${getRandomSize()}`}
+            onClick={handleImageClick(pins[item.id])}
+          >
             <img src={pins[item.id]?.imgUrl} alt="No pin" />
           </div>
         ))}
       </div>
+      {showModal && selectedPin && (
+        <Modal onClose={handleModalClose}>
+          <ShowPinItem pin={selectedPin} />
+        </Modal>
+      )}
     </>
   );
 }
